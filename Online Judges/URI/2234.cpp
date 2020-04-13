c941849f6923072ea9bb277a5876850363d6ae31
Ascanio Savio <ascaniosavio97@gmail.com>
2020-04-12 22:19:17 -0300

    Add files via upload

---
+++ Online Judges/URI/2234.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+
+int main()
+{
+  int h, p;
+  scanf("%d%d", &h, &p);
+  printf("%.2lf\n", h / (double)p);
+  return 0;
+}
