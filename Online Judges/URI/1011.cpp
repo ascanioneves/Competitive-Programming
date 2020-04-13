90f0557f508ad94be94fef962ee2abbd13c7dc06
Ascanio Savio <ascaniosavio97@gmail.com>
2020-04-12 22:35:08 -0300

    Add files via upload

---
+++ Online Judges/URI/1011.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double pi = 3.14159;
+
+int main()
+{
+  double r; scanf("%lf", &r);
+  printf("VOLUME = %.3lf\n", (4 * pi * r * r * r) / 3.0);
+  return 0;
+}
